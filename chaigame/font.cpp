95f54c742f599381bb9db329c916a1887dfca10c
Rob Loach <robloach@gmail.com>
2017-07-15 00:56:13 -0400

    Update error handling

--- chaigame/font.cpp
+++ chaigame/font.cpp
@@ -9,8 +9,13 @@ namespace chaigame {
 	bool font::load() {
 		int result = TTF_Init();
 		if(result == -1) {
-			log()->error("TTF_Init failed to initialize font system: {}", TTF_GetError());
-		    return false;
+			const char* errorChar = TTF_GetError();
+			std::string errString("");
+			if (errorChar != NULL) {
+				errString = errorChar;
+			}
+			log()->error("TTF_Init failed to initialize font system: {}", errString());
+			return false;
 		}
 		return true;
 	}
--- chaigame/sound.cpp
+++ chaigame/sound.cpp
@@ -13,8 +13,13 @@ namespace chaigame {
 		int flags = MIX_INIT_OGG | MIX_INIT_MOD;
 		int initted = Mix_Init(flags);
 		if ((initted & flags) != flags) {
-		    log()->error("[sound] Mix_Init() failed requiring ogg and mod support: {}", Mix_GetError());
-		    return false;
+			const char* errorChar = Mix_GetError();
+			std::string errString("");
+			if (errorChar != NULL) {
+				errString = errorChar;
+			}
+			log()->error("[sound] Mix_Init() failed requiring ogg and mod support: {}", errString);
+			return false;
 		}
 		return loaded = true;
 	}
@@ -27,30 +32,40 @@ namespace chaigame {
 		if (firstRun && toInit) {
 			firstRun = false;
 			if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024) == -1) {
-				log()->error("Mix_OpenAudio: {}", Mix_GetError());
+				const char* errorChar = Mix_GetError();
+				std::string errString("");
+				if (errorChar != NULL) {
+					errString = errorChar;
+				}
+				log()->error("Mix_OpenAudio: {}", errString);
 				return false;
 			}
 
-		    numtimesopened = Mix_QuerySpec(&frequency, &format, &channels);
-		    if (!numtimesopened) {
-		    	log()->error("Mix_QuerySpec: {}", Mix_GetError());
-		    	return false;
-		    }
+			numtimesopened = Mix_QuerySpec(&frequency, &format, &channels);
+			if (!numtimesopened) {
+				const char* errorChar = Mix_GetError();
+				std::string errString("");
+				if (errorChar != NULL) {
+					errString = errorChar;
+				}
+				log()->error("Mix_QuerySpec: {}", errString);
+				return false;
+			}
 			initialized = true;
-	    	std::string format_str="Unknown";
-	    	switch(format) {
-	    	    case AUDIO_U8: format_str="U8"; break;
-	    	    case AUDIO_S8: format_str="S8"; break;
-	    	    case AUDIO_U16LSB: format_str="U16LSB"; break;
-	    	    case AUDIO_S16LSB: format_str="S16LSB"; break;
-	    	    case AUDIO_U16MSB: format_str="U16MSB"; break;
-	    	    case AUDIO_S16MSB: format_str="S16MSB"; break;
-	    	}
-	    	// printf("\n  opened=%d times\n  frequency=%dHz\n  format=%s\n  channels=%d\n", numtimesopened, frequency, format_str.c_str(), channels);
+			std::string format_str="Unknown";
+			switch(format) {
+				case AUDIO_U8: format_str="U8"; break;
+				case AUDIO_S8: format_str="S8"; break;
+				case AUDIO_U16LSB: format_str="U16LSB"; break;
+				case AUDIO_S16LSB: format_str="S16LSB"; break;
+				case AUDIO_U16MSB: format_str="U16MSB"; break;
+				case AUDIO_S16MSB: format_str="S16MSB"; break;
+			}
+			// printf("\n  opened=%d times\n  frequency=%dHz\n  format=%s\n  channels=%d\n", numtimesopened, frequency, format_str.c_str(), channels);
 
-	    }
+		}
 
-	    return initialized;
+		return initialized;
 	}
 
 	void sound::unload() {
@@ -73,13 +88,13 @@ namespace chaigame {
 	}
 
 	SoundData* sound::newSoundData(const std::string& file, const std::string& type) {
- 		SDL_RWops* rw = Application::getInstance()->filesystem.openRW(file);
- 		if (rw) {
- 			SoundData* newSound = new SoundData(rw, type);
- 			sounds.push_back(newSound);
- 			return newSound;
- 		}
- 		log()->error("Error loading newSoundData('{}', '{}')", file, type);
+		SDL_RWops* rw = Application::getInstance()->filesystem.openRW(file);
+		if (rw) {
+			SoundData* newSound = new SoundData(rw, type);
+			sounds.push_back(newSound);
+			return newSound;
+		}
+		log()->error("Error loading newSoundData('{}', '{}')", file, type);
 	}
 
 	SoundData* sound::newSoundData(const std::string& file) {
