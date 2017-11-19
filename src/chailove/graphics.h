56587bbca591dc5dc74284453c4780f9c42acedf
Rob Loach <robloach@gmail.com>
2017-11-19 01:41:00 -0500

    Update doc blocksg

--- src/chailove/graphics.h
+++ src/chailove/graphics.h
@@ -22,7 +22,7 @@ namespace chailove {
 		bool load();
 
 		/**
-		 * Draws a rectangle.
+		 * @brief Draws a rectangle.
 		 *
 		 * @param drawmode How to draw the rectangle. Can be "fill" or "line".
 		 * @param x The position of top-left corner along the x-axis.
@@ -35,68 +35,128 @@ namespace chailove {
 		 * @endcode
 		 */
 		void rectangle(const std::string& drawmode, int x, int y, int width, int height);
+
+		/**
+		 * @brief Clears the screen to the set background color.
+		 */
 		void clear();
+
+		/**
+		 * @brief Clears the screen to the given background color.
+		 */
 		void clear(int r, int g, int b, int a);
+
+		/**
+		 * @brief Clears the screen to the given background color.
+		 */
 		void clear(int r, int g, int b);
+
+		/**
+		 * @brief Draws a point.
+		 */
 		void point(int x, int y);
+
+		/**
+		 * @brief Draws a point.
+		 */
 		void point(Point* p);
 		//void points(std::vector<Point*>);
 		//void points(std::vector<Point>);
+
+		/**
+		 * @brief Draws a line.
+		 */
 		void line(int x1, int y1, int x2, int y2);
 
+
+		/**
+		 * @brief Draws an image on screen.
+		 */
 		void draw(Image* image, int x, int y);
+
+		/**
+		 * @brief Draws an image on screen, using the given Quad as a source.
+		 */
 		void draw(Image* image, Quad quad, int x, int y);
 
+		/**
+		 * @brief Creates a new Quad.
+		 */
 		Quad newQuad(int x, int y, int width, int height, int sw, int sh);
 
+		/**
+		 * @brief Creates a new Image.
+		 */
 		Image* newImage(const std::string& filename);
+
 		Font* newFont();
+		/**
+		 * @brief Creates a new Font from a TrueType Font or BMFont file.
+		 */
 		Font* newFont(const std::string& filename);
+		/**
+		 * @brief Creates a new Font from a TrueType Font or BMFont file.
+		 */
 		Font* newFont(const std::string& filename, int size);
+		/**
+		 * @brief Creates a new BMFont.
+		 */
 		Font* newFont(const std::string& filename, int glyphWidth, int glyphHeight, const std::string& letters);
+
+		/**
+		 * @brief Set an already-loaded Font as the current font.
+		 */
 		void setFont(Font* font);
 		void setFont();
 		Font* getFont();
 
 		/**
-		 * Prints the given text to the screen.
+		 * @brief Prints the given text to the screen.
 		 */
 		void print(const std::string& text, int x, int y);
 
 		/**
-		 * Sets the active drawing color to the given color.
+		 * @brief Sets the active drawing color to the given color.
 		 */
 		void setColor(int red, int green, int blue, int alpha);
 		/**
-		 * Sets the active drawing color to the given color.
+		 * @brief Sets the active drawing color to the given color.
 		 */
 		void setColor(int red, int green, int blue);
 		/**
-		 * Sets the background color to the given color.
+		 * @brief Sets the background color to the given color.
 		 */
 		void setBackgroundColor(int red, int green, int blue, int alpha);
 		/**
-		 * Sets the background color to the given color.
+		 * @brief Sets the background color to the given color.
 		 */
 		void setBackgroundColor(int red, int green, int blue);
+
+		/**
+		 * @brief Retrieve the width of the screen.
+		 */
+		int getWidth();
+		/**
+		 * @brief Retrieve the height of the screen.
+		 */
+		int getHeight();
+
+		/**
+		 * @brief Draws a circle.
+		 */
+		void circle(const std::string& drawmode, int x, int y, int radius);
+		/**
+		 * @brief Draws an ellipse.
+		 */
+		void ellipse(const std::string& drawmode, int x, int y, int radiusx, int radiusy);
+		/**
+		 * @brief Draws an arc.
+		 */
+		void arc(const std::string& drawmode, int x, int y, int radius, int angle1, int angle2);
 
 		Uint8 r = 255, g = 255, b = 255, a = 255;
 		Uint8 backR = 0, backG = 0, backB = 0, backA = 255;
 		SDL_Surface* getScreen();
-
-		/**
-		 * Retrieve the width of the screen.
-		 */
-		int getWidth();
-		/**
-		 * Retrieve the height of the screen.
-		 */
-		int getHeight();
-
-		void circle(const std::string& drawmode, int x, int y, int radius);
-		void ellipse(const std::string& drawmode, int x, int y, int radiusx, int radiusy);
-		void arc(const std::string& drawmode, int x, int y, int radius, int angle1, int angle2);
-
 		Font* activeFont;
 		Font defaultFont;
 	};
