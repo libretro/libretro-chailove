14b840b58993ba4eb93b683d693becce39faa553
Rob Loach <robloach@gmail.com>
2017-11-25 20:53:55 -0500

    Add zlib to compress the state string

--- src/Modules/math.h
+++ src/Modules/math.h
@@ -1,6 +1,8 @@
 #ifndef CHAILOVE_MATH_H
 #define CHAILOVE_MATH_H
 
+#include <string>
+
 namespace Modules {
 	/**
 	 * @brief Provides system-independent mathematical functions.
@@ -32,6 +34,24 @@ namespace Modules {
 		 * @brief Gets the seed of the random number generator.
 		 */
 		int getRandomSeed();
+
+		/**
+		 * @brief Compress the given string.
+		 *
+		 * @param str The string to compress.
+		 *
+		 * @return Compressed data in the form of a string.
+		 */
+		std::string compress(const std::string& str);
+
+		/**
+		 * @brief Decompress the given string.
+		 *
+		 * @param str The string to decompress.
+		 *
+		 * @return The decompressed string.
+		 */
+		std::string decompress(const std::string& str);
 
 		/**
 		 * @brief Pi
