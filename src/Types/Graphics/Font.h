9abb4efba2267afbb5f3064b64467c0e73e7c205
Rob Loach <robloach@gmail.com>
2017-11-27 22:38:52 -0500

    Add TrueType font loading

--- src/Types/Graphics/Font.h
+++ src/Types/Graphics/Font.h
@@ -2,7 +2,7 @@
 #define CHAILOVE_UTILITY_FONT_H
 
 #include "SDL.h"
-//#include "SDL_ttf.h"
+#include "SDL_ttf.h"
 #include <string>
 #include <SDL_fnt.h>
 #include "Image.h"
@@ -21,7 +21,7 @@ namespace Types {
 		bool loaded();
 		bool destroy();
 		void print(const std::string& text, int x, int y, int r, int g, int b, int a);
-		//TTF_Font* ttfFont = NULL;
+		TTF_Font* ttfFont = NULL;
 		TTY_Font* ttyFont = NULL;
 		int ttyFontWidth, ttyFontHeight;
 		/**
