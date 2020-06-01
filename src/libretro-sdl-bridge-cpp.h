30f1ab3e1d4091ed706a7e8377662f214b05fdf9
Rob Loach <robloach@gmail.com>
2020-06-01 01:14:37 -0400

    Revert "bridge?"
    This reverts commit 83be3e01505632dde4074838a77572925c94b7f3.

--- src/libretro-sdl-bridge-cpp.h
+++
@@ -1,9 +0,0 @@
-#ifndef LIBRETRO_SDL_BRIDGE_CPP_H__
-#define LIBRETRO_SDL_BRIDGE_CPP_H__
-
-/**
- * libretro-sdl callback; Send through the input state.
- */
-short int libretro_input_state_cb_cpp(unsigned port, unsigned device, unsigned index, unsigned id);
-
-#endif
